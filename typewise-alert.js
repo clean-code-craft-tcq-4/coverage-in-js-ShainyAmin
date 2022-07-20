89930b882a2c141f2b148a1a54f7aee480f40f97
ShainyAmin <38464281+ShainyAmin@users.noreply.github.com>
2022-07-20 22:29:53 +0530

    Add files via upload

--- typewise-alert.js
+++ typewise-alert.js
@@ -1,54 +1,19 @@
-
-function inferBreach(value, lowerLimit, upperLimit) {
-  if (value < lowerLimit) {
-    return 'TOO_LOW';
-  }
-  if (value > upperLimit) {
-    return 'TOO_HIGH';
-  }
-  return 'NORMAL';
-}
-
-function classifyTemperatureBreach(coolingType, temperatureInC) {
-  let lowerLimit = 0;
-  let upperLimit = 0;
-  if (coolingType == 'PASSIVE_COOLING') {
-    lowerLimit = 0;
-    upperLimit = 35;
-  } else if (coolingType == 'HI_ACTIVE_COOLING') {
-    lowerLimit = 0;
-    upperLimit = 45;
-  } else if (coolingType == 'MED_ACTIVE_COOLING') {
-    lowerLimit = 0;
-    upperLimit = 40;
-  }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
-}
+const controllerAlert = require('./controller-alert');
+const emailAlert = require('./email-alert');
+const tempBreach = require('./typewise-temp-breach');
 
 function checkAndAlert(alertTarget, batteryChar, temperatureInC) {
-  const breachType = classifyTemperatureBreach(batteryChar['coolingType'], temperatureInC);
+  const breachType = tempBreach.classifyTemperatureBreach(batteryChar, temperatureInC);
+  let status = false;
   if (alertTarget == 'TO_CONTROLLER') {
-    sendToController(breachType);
+    status = controllerAlert.sendToController(breachType);
+    return status;
   } else if (alertTarget == 'TO_EMAIL') {
-    sendToEmail(breachType);
+    status = emailAlert.sendToEmail(breachType);
+    return status;
   }
+  return false;
 }
+module.exports = {checkAndAlert};
 
-function sendToController(breachType) {
-  const header = 0xfeed;
-  console.log(`${header}, ${breachType}`);
-}
 
-function sendToEmail(breachType) {
-  const recepient = 'a.b@c.com';
-  if (breachType == 'TOO_LOW') {
-    console.log(`To: ${recepient}`);
-    console.log('Hi, the temperature is too low');
-  } else if (breachType == 'TOO_HIGH') {
-    console.log(`To: ${recepient}`);
-    console.log('Hi, the temperature is too high');
-  }
-}
-
-module.exports =
-    {inferBreach, classifyTemperatureBreach, checkAndAlert, sendToController, sendToEmail};
